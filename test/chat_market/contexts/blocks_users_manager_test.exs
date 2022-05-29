677ff58b9ce2f811f1928d363d906594f708de5e
Sergio Valente <plusrt1@gmail.com>
2022-05-28 23:16:15 -0500

    feat: add blocks users manager

---
+++ test/chat_market/contexts/blocks_users_manager_test.exs
@@ -0,0 +1,17 @@
+defmodule ChatMarket.Contexts.BlocksUsersManagerTest do
+	use ChatMarket.DataCase, async: true
+
+	alias ChatMarket.Contexts.BlocksUsersManager
+
+  test "create/1 with rigth" do
+    user = insert(:user)
+    block = insert(:block)
+
+    params = %{
+      user_id: user.id,
+      block_id: block.id
+    }
+
+    assert {:ok, _blocks_users} = BlocksUsersManager.create(params)
+  end
+end
