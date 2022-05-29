cbe95ad4afe2100b1c0a6366b1432450d89e70f4
Sergio Valente <plusrt1@gmail.com>
2022-05-29 02:33:20 -0500

    feat: when users mark block as available is redirected to available blocks list

--- lib/chat_market_web/controllers/switch_request_controller.ex
+++ lib/chat_market_web/controllers/switch_request_controller.ex
@@ -8,7 +8,7 @@ defmodule ChatMarketWeb.SwitchRequestController do
   def create(conn, %{"blocks_users_id" => blocks_users_id}) do
     with %BlocksUsers{} = block_user <- BlocksUsersManager.get(blocks_users_id) |> IO.inspect(label: "GET  BLOCK"),
     {:ok, %BlocksUsers{} = _updated_block_user} <- BlocksUsersManager.update(block_user, %{status: :available}) |> IO.inspect(label: "UPDATE BLOCK") do
-      redirect(conn, to: Routes.block_path(conn, :index))
+      redirect(conn, to: Routes.available_block_path(conn, :index))
     end
   end
 end
