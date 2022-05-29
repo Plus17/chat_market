d0a3e4de0d2eb410985d68bdb2a0ab058f814851
Sergio Valente <plusrt1@gmail.com>
2022-05-28 21:08:37 -0500

    chore: format file

--- lib/chat_market_web/controllers/users/block_controller.ex
+++ lib/chat_market_web/controllers/users/block_controller.ex
@@ -4,8 +4,7 @@ defmodule ChatMarketWeb.BlockController do
   alias ChatMarket.Blocks
 
   def index(conn, _params) do
-    blocks =
-      Blocks.list_block_by_user_id(conn.assigns.current_user.id)
+    blocks = Blocks.list_block_by_user_id(conn.assigns.current_user.id)
 
     render(conn, "index.html", blocks: blocks)
   end
