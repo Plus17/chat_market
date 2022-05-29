76b9289eb826270687646fe98ebeeef82126b349
Sergio Valente <plusrt1@gmail.com>
2022-05-29 02:45:18 -0500

    chore: remove inspects

--- lib/chat_market_web/controllers/switch_request_controller.ex
+++ lib/chat_market_web/controllers/switch_request_controller.ex
@@ -6,8 +6,8 @@ defmodule ChatMarketWeb.SwitchRequestController do
   alias ChatMarket.Contexts.BlocksUsersManager
 
   def create(conn, %{"blocks_users_id" => blocks_users_id}) do
-    with %BlocksUsers{} = block_user <- BlocksUsersManager.get(blocks_users_id) |> IO.inspect(label: "GET  BLOCK"),
-    {:ok, %BlocksUsers{} = _updated_block_user} <- BlocksUsersManager.update(block_user, %{status: :available}) |> IO.inspect(label: "UPDATE BLOCK") do
+    with %BlocksUsers{} = block_user <- BlocksUsersManager.get(blocks_users_id),
+    {:ok, %BlocksUsers{} = _updated_block_user} <- BlocksUsersManager.update(block_user, %{status: :available}) do
       redirect(conn, to: Routes.available_block_path(conn, :index))
     end
   end
