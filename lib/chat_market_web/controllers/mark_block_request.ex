761b9485b7b23d20133f4afda69862b752e8fa2f
Sergio Valente <plusrt1@gmail.com>
2022-05-29 04:24:09 -0500

    feat: mark block as available

--- lib/chat_market_web/controllers/mark_block_request.ex
+++ lib/chat_market_web/controllers/mark_block_request.ex
@@ -7,8 +7,8 @@ defmodule ChatMarketWeb.MarkBlockController do
 
   def create(conn, %{"blocks_users_id" => blocks_users_id}) do
     with %BlocksUsers{} = block_user <- BlocksUsersManager.get(blocks_users_id) |> IO.inspect(label: "GET  BLOCK"),
-    {:ok, %BlocksUsers{} = _updated_block_user} <- BlocksUsersManager.update(block_user, %{status: :available}) |> IO.inspect(label: "UPDATE BLOCK") do
-      redirect(conn, to: Routes.available_block_path(conn, :index))
+    {:ok, %BlocksUsers{} = updated_block_user} <- BlocksUsersManager.update(block_user, %{status: :available}) |> IO.inspect(label: "UPDATE BLOCK") do
+      redirect(conn, to: Routes.available_block_path(conn, :index, changed_block_user: updated_block_user))
     end
   end
 end
